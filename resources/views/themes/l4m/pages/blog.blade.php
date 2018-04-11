98ec619d61c086c40b471a08557b17cd66be0812
VacaSan <kotaracvladan@gmail.com>
2018-04-11 16:02:26 +0200

    section title and grid updates

--- resources/views/themes/l4m/pages/blog.blade.php
+++ resources/views/themes/l4m/pages/blog.blade.php
@@ -1,5 +1,38 @@
 @extends('themes.l4m.index')
 @section('content')
-  <h1>hello, world</h1>
-  <button class="btn btn-primary">button</button>
+<style>
+  .test {
+    height: 300px;
+    width: 100%;
+    background-color: aqua;
+  }
+  .banner {
+    height: 300px;
+    background-color: chocolate;
+  }
+  .hide {
+    display: none;
+  }
+</style>
+  <div class="container">
+    <h1 class="section-title"><span>section title</span></h1>
+    <div class="grid">
+      <div class="row">
+        <div class="column">
+          <div class="test">column</div>
+        </div>
+        <div class="column">
+          <div class="test">column</div>
+        </div>
+        <div class="column">
+          <div class="test">column</div>
+        </div>
+      </div>
+      <div class="row" id="banner">
+        <div class="column">
+          <div class="banner">banner</div>
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
