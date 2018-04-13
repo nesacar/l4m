77dea14167f2f1c27b972eaee5e1f78e5e344b22
VacaSan <kotaracvladan@gmail.com>
2018-04-13 17:03:52 +0200

    added scss functions

--- resources/views/themes/l4m/pages/blog.blade.php
+++ resources/views/themes/l4m/pages/blog.blade.php
@@ -11,31 +11,40 @@
     flex-shrink: 0;
     /* temp */
     height: 450px;
-    background-color: lightgray;
+    background-color: #E0E0E0;
   }
-  .test {
-    background-color: darkgray;
-    height: 320px;
+  .demo {
+    background-color: #9E9E9E;
+  }
+  .demo-img {
+    background-color: #424242;
   }
   .hide {
     display: none;
   }
 </style>
-  <div class="container">
+  <section class="container">
     <h1 class="section-title"><span>section title</span></h1>
     <div class="grid-wrap">
       <div class="grid">
-        <div class="grid_item test">
+        <div class="grid_item demo">
+          <div class="image image--standard demo-img"></div>
           grid item
         </div>
-        <div class="grid_item test">
+        <div class="grid_item demo">
+          <div class="image image--standard demo-img"></div>
           grid item
         </div>
-        <div class="grid_item test">
+        <div class="grid_item demo">
+          <div class="image image--standard demo-img"></div>
           grid item
         </div>
       </div>
-      <div class="banner" id="banner"></div>
+      <aside class="banner" id="banner">banner positions</aside>
     </div>
-  </div>
+  </section>
+
+  <section class="container">
+    <h1 class="section-title"><span>section title</span></h1>
+  </section>
 @endsection
