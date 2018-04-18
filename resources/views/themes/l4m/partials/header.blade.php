1554ea791c84e035c1f417ac9cb6a34ffb493c2b
VacaSan <kotaracvladan@gmail.com>
2018-04-18 16:59:07 +0200

    Header and logo styles

--- resources/views/themes/l4m/partials/header.blade.php
+++ resources/views/themes/l4m/partials/header.blade.php
@@ -1,7 +1,11 @@
-<header id="header">
-  <div class="container header">
-    <div>
-      logo...
+<header class="container" id="header">
+  <div class="header">
+    <div class="header_content">
+      <a class="header_logo-container with-flare" href="/">
+        <svg class="header_logo" role="presentation">
+          <use xlink:href="#logo">
+        </svg>
+      </a>
     </div>
     @include('themes.'.env('THEME_NAME', '').'.partials.nav')
   </div>
