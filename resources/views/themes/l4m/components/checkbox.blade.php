a2542606c82edb18c2ba133088ea4293155b9337
VacaSan <kotaracvladan@gmail.com>
2018-04-24 15:35:09 +0200

    Add filter checkboxes

---
+++ resources/views/themes/l4m/components/checkbox.blade.php
@@ -0,0 +1,9 @@
+<div class="checkbox">
+  <input class="checkbox_control" id="{{ $id }}" type="checkbox" {{ $checked ? 'checked' : '' }} />
+  <div class="checkbox_background">
+    <svg class="checkbox_checkmark" viewBox="0 0 24 24">
+      <path class="checkbox_path" fill="none" stroke="white" d="M1.73,12.91 8.1,19.28 22.79,4.59"></path>
+    </svg>
+  </div>
+</div>
+<label class="checkbox_label" title="{{ $value }}" for="{{ $id }}">{{ $value }}</label>
--- resources/views/themes/l4m/partials/filters.blade.php
+++ resources/views/themes/l4m/partials/filters.blade.php
@@ -1,1 +1,15 @@
-<div>filters</div>
+<div class="filters">
+  <div class="filter">
+    <h4 class="filter_name">filter name</h4>
+    <ul class="filter-list">
+      <li class="filter-list_item">
+        @component('themes.' . env('THEME_NAME', '') . '.components.checkbox', [
+          'id' => 'ch-1',
+          'checked' => false,
+          'value' => 'Filter value'
+        ])
+        @endcomponent
+      </li>
+    </ul>
+  </div>
+</div>
