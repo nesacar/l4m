4c37e4c794016552f56934e1f2c6150b2bc57ce8
nesacar <nebojsart1409@yahoo.com>
2018-05-16 09:37:22 +0200

    breadcrumbs part 1

--- resources/views/themes/l4m/partials/breadcrumb.blade.php
+++ resources/views/themes/l4m/partials/breadcrumb.blade.php
@@ -1,3 +1,11 @@
 @if(!empty($breadcrumb))
   {!! $breadcrumb !!}
+@else
+<nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="#">Home</a></li>
+    <li class="breadcrumb-item"><a href="#">Shop</a></li>
+    <li class="breadcrumb-item active" aria-current="page">Fashion</li>
+  </ol>
+</nav>
 @endif
