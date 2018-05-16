f7c2d6cbfae7884aaf2cb98d471700ecfce3207e
VacaSan <kotaracvladan@gmail.com>
2018-05-16 15:28:13 +0200

    Redesign image box.

--- resources/views/themes/l4m/components/image-box.blade.php
+++ resources/views/themes/l4m/components/image-box.blade.php
@@ -1,24 +1,22 @@
-<div class="row image-box">
-  <div class="col-lg-3">
-    <div class="image-box_thumbnails image-gallery_thumbnails">
-      
-      <div class="image-box_thumbnail">
-        <div class="image image--portrait">
-          <img class="active" src="{{ \Imagecache::get($image, '90x120')->src }}" alt="{{ $title }}" data-large="{{ url($image) }}">
-        </div>
+<div class="image-box">
+  <div class="image-box_thumbnails image-gallery_thumbnails">
+    
+    <div class="image-box_thumbnail">
+      <div class="image image--portrait">
+        <img class="active" src="{{ \Imagecache::get($image, '90x120')->src }}" alt="{{ $title }}" data-large="{{ url($image) }}">
       </div>
-      @if(count($photos))
-      @foreach ($photos as $photo)
-      <div class="image-box_thumbnail">
-        <div class="image image--portrait">
-          <img src="{{ url($photo->tmb) }}" alt="{{ $title }}" data-large="{{ url($photo->file_path) }}">
-        </div>
-      </div>
-      @endforeach
-      @endif
     </div>
+    @if(count($photos))
+    @foreach ($photos as $photo)
+    <div class="image-box_thumbnail">
+      <div class="image image--portrait">
+        <img src="{{ url($photo->tmb) }}" alt="{{ $title }}" data-large="{{ url($photo->file_path) }}">
+      </div>
+    </div>
+    @endforeach
+    @endif
   </div>
-  <div class="col-lg-9 image-box_image">
+  <div class="image-box_image">
     <div class="shop-item_image" style="position: relative;">
       <div class="image image--portrait">
         <img src="{{ url($image) }}" class="zoomer-target image-gallery_target" draggable="false"/>
