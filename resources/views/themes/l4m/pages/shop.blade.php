28e0ae6d69687da53d2adf18442bb6c18c6257f7
VacaSan <kotaracvladan@gmail.com>
2018-05-17 12:47:21 +0200

    Yoox shop header applied.

--- resources/views/themes/l4m/pages/shop.blade.php
+++ resources/views/themes/l4m/pages/shop.blade.php
@@ -17,7 +17,56 @@
       ])
       @endcomponent
 
-      @include('themes.' . $theme . '.partials.breadcrumb')
+      {{-- @include('themes.' . $theme . '.partials.breadcrumb') --}}
+
+      <div class="shop-results_header">
+        {{-- <div class="shop-results_count">Showing 1-15 of {{ $data['count'] }} results</div> --}}
+        {{-- @select([ 'name' => 'sort', 'id' => 'sort', 'form' => 'filters' ])
+          <option value="1" {{ request('sort') == 1? 'selected' : '' }}>Najnovije</option>
+          <option value="2" {{ request('sort') == 2? 'selected' : '' }}>Cena rastuce</option>
+          <option value="3" {{ request('sort') == 3? 'selected' : '' }}>Cena opadajuce</option>
+        @endselect --}}
+
+        <div class="shop_sort-options">
+          <div class="shop_sort-label">Prikaži po:</div>
+          <div class="shop_sort-option">
+            <input
+              type="radio"
+              name="sort"
+              id="najnovije"
+              value="1"
+              form="filters"
+              {{ request('sort') == 1? 'checked' : '' }}
+            >
+            <label for="najnovije">Najnovije</label>
+          </div>
+          <div class="shop_sort-option">
+            <input
+              type="radio"
+              name="sort"
+              id="rastuce"
+              value="2"
+              form="filters"
+              {{ request('sort') == 2? 'checked' : '' }}
+            >
+            <label for="rastuce">Cena rastuće</label>
+          </div>
+          <div class="shop_sort-option">
+            <input
+              type="radio"
+              name="sort"
+              id="opadajuce"
+              value="3"
+              form="filters"
+              {{ request('sort') == 3? 'checked' : '' }}
+            >
+            <label for="opadajuce">Cena opadajuće</label>
+          </div>
+        </div>
+
+        {{ $data['products']->appends(Request::all())->links() }}
+
+      </div>
     
       <div class="row">
         <aside class="col-lg-3 filters-container">
@@ -25,15 +74,6 @@
         </aside>
 
         <div class="col-lg-9">
-          <div class="shop-results_header">
-            <div class="shop-results_count">Showing 1-15 of {{ $data['count'] }} results</div>
-            @select([ 'name' => 'sort', 'id' => 'sort', 'form' => 'filters' ])
-              <option value="1" {{ request('sort') == 1? 'selected' : '' }}>Najnovije</option>
-              <option value="2" {{ request('sort') == 2? 'selected' : '' }}>Cena rastuce</option>
-              <option value="3" {{ request('sort') == 3? 'selected' : '' }}>Cena opadajuce</option>
-            @endselect
-
-          </div>
 
           @component('themes.' . $theme . '.components.shop.grid', [
             'component' => 'shop.item',
