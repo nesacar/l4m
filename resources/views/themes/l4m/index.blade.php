aa13ef4b33af51b218b97eb09154c41160b422ba
VacaSan <kotaracvladan@gmail.com>
2018-05-17 14:55:21 +0200

    Embed fonts.

--- resources/views/themes/l4m/index.blade.php
+++ resources/views/themes/l4m/index.blade.php
@@ -5,6 +5,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   @yield('title')
+  <link href="https://fonts.googleapis.com/css?family=Lora&amp;subset=latin-ext" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&amp;subset=latin-ext" rel="stylesheet">
   <link rel="stylesheet" href="{{ url('themes/' . $theme . '/css/main.css') }}">
   @yield('seo')
   @yield('styles')
--- resources/views/themes/l4m/pages/product.blade.php
+++ resources/views/themes/l4m/pages/product.blade.php
@@ -10,19 +10,31 @@
         @include('themes.' . $theme . '.partials.breadcrumb')
 
         <div class="row">
+            <div class="col-12 product_details--mobile">
+                <h1 class="display-4 product_name">{{ $product->brand->title }}</h1>
+                <div class="product_brand">{{ $product->title }}</div>
+                <div class="display-4 product_price">
+                    @if($product->discount)
+                        <span class="shop-item_price-tag shop-item_price-tag--invalid">{{ number_format($product->price, 2, ',', '.') }}</span>
+                    @endif
+                    <span class="shop-item_price-tag">{{ $product->totalPrice }}</span>
+                </div>
+            </div>
             <div class="col-md-6">
                 @imagebox([ 'image' => $product->image, 'tmb' => $product->tmb, 'title' => $product->title, 'photos' => $product->photo ])@endimagebox
             </div>
             <div class="col-md-6">
 
                 <div class="product_details">
-                    <h1 class="display-4 product_name">{{ $product->brand->title }}</h1>
-                    <div class="product_brand">{{ $product->title }}</div>
-                    <div class="display-4 product_price">
-                        @if($product->discount)
-                            <span class="shop-item_price-tag shop-item_price-tag--invalid">{{ number_format($product->price, 2, ',', '.') }}</span>
-                        @endif
-                        <span class="shop-item_price-tag">{{ $product->totalPrice }}</span>
+                    <div>
+                        <h1 class="display-4 product_name">{{ $product->brand->title }}</h1>
+                        <div class="product_brand">{{ $product->title }}</div>
+                        <div class="display-4 product_price">
+                            @if($product->discount)
+                                <span class="shop-item_price-tag shop-item_price-tag--invalid">{{ number_format($product->price, 2, ',', '.') }}</span>
+                            @endif
+                            <span class="shop-item_price-tag">{{ $product->totalPrice }}</span>
+                        </div>
                     </div>
                     <div class="product_options">
                         @if(false)
