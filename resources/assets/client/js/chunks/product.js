66c278f4889aeac32fd67bdd57a0128934cbd905
VacaSan <kotaracvladan@gmail.com>
2018-05-18 16:31:40 +0200

    Added ImageGallery API

--- resources/assets/client/js/chunks/product.js
+++ resources/assets/client/js/chunks/product.js
@@ -2,9 +2,14 @@ import ImageZoomer from '../components/image-zoomer';
 import ImageGallery from '../components/image-gallery';
 
 (function() {
+  
   const zoomer = new ImageZoomer();
-  ImageGallery.init((src) => {
-    zoomer.init();
+  window.imgGallery = new ImageGallery({
+    onChange,
   });
+
+  function onChange() {
+    zoomer.init();
+  }
 }());
 
--- resources/assets/client/js/components/image-gallery.js
+++ resources/assets/client/js/components/image-gallery.js
@@ -7,18 +7,26 @@ class ImageGallery {
   }
 
   /**
-   * Sets up the image gallery.
-   *
-   * @param {Function} callback - Function to call on every image swap
+   * @type {Object}
    */
-  static init(callback=function() {}) {
-    new ImageGallery();
+  static get CALLBACKS() {
+    return {
+      onChange: function() {},
+    };
   }
 
-  constructor() {
+  /**
+   * Creates new ImageGallery instance.
+   *
+   * @param {Object} callbacks - Callbacks object.
+   * @prop {Function} callbacks.onChange - Gets called on state change.
+   */
+  constructor(callbacks) {
     this.state = {
       activeIndex: 0,
     };
+
+    this.callbacks = Object.assign(ImageGallery.CALLBACKS, callbacks);
 
     this.target = document.querySelector('.image-gallery_target');
     this.images = document.querySelectorAll('.image-gallery_thumbnails img');
@@ -28,6 +36,44 @@ class ImageGallery {
     });
   }
 
+  /**
+   * Sets image at given index as active.
+   * 
+   * @param {Number} index - Index of image to render.
+   */
+  goTo(index) {
+    const size = this.images.length;
+
+    if (index >= size) {
+      index = 0;
+    } else if (index < 0) {
+      index = size - 1;
+    }
+
+    this.setState({
+      activeIndex: index,
+    });
+  }
+
+  /**
+   * Sets next image as active.
+   */
+  nextImage() {
+    this.goTo(this.state.activeIndex + 1);
+  }
+
+  /**
+   * Sets prev image as acitve.
+   */
+  prevImage() {
+    this.goTo(this.state.activeIndex - 1);
+  }
+
+  /**
+   * Image click handler.
+   *
+   * @param {Number} index - Index of the element.
+   */
   _onClick(index) {
     return (evt) => {
       this.setState({
@@ -36,8 +82,32 @@ class ImageGallery {
     };
   }
 
-  update() {
-    // TODO: Split in different methods.
+  /**
+   * Swaps the target image with the selected image.
+   *
+   * @private
+   */
+  _applyImage() {
+    const index = this.state.activeIndex;
+    const img = this.images[index];
+    const src = img.dataset.large;
+    // Apply small image first.
+    this.target.src = img.src;
+
+    const _img = new Image();
+    _img.src = src;
+    _img.onload = () => {
+      this.target.src = src;
+      this.callbacks.onChange();
+    };
+  }
+
+  /**
+   * Sets the styling to thumbnails to match the state.
+   *
+   * @private
+   */
+  _markActiveThumbnail() {
     const index = this.state.activeIndex;
 
     this.images.forEach((img, i) => {
@@ -47,25 +117,30 @@ class ImageGallery {
       }
       img.classList.remove(ImageGallery.ACTIVE_CLASS);
     });
-
-    const img = this.images[index];
-    const src = img.dataset.large;
-
-    // Apply small image first.
-    this.target.src = img.src;
-
-    const _img = new Image();
-    _img.src = src;
-    _img.onload = () => {
-      this.target.src = src;
-    };
   }
 
+  /**
+   * Fires on every state change, and updates the ui.
+   */
+  update() {
+    this._markActiveThumbnail();
+    this._applyImage();
+  }
+
+  /**
+   * Updates the state of the component, and forces the
+   * re-render.
+   *
+   * @param {any} obj - Sate change.
+   */
   setState(obj) {
     this.state = Object.assign({}, this.state, obj);
     this.__render();
   }
 
+  /**
+   * @private
+   */
   __render() {
     if (!this.update) {
       throw new Error('update method must be defined.');
