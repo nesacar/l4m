ecd0049a6f07e9c1f9afb56ddf5cecf6a8bf82f5
VacaSan <kotaracvladan@gmail.com>
2018-05-22 10:08:27 +0200

    "Brand" section.

--- resources/views/themes/l4m/pages/client/home.blade.php
+++ resources/views/themes/l4m/pages/client/home.blade.php
@@ -49,5 +49,47 @@
     </div>
   </div>
 </div>
+
+<div class="container client_products">
+  <h2 class="display-3 section-title--serif">Akcija</h2>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+  <div class="showcase_carousel client_showcase">
+    {{-- @foreach($featuredProducts->first()->product as $i => $product)
+    <div class="showcase_item">
+      @component('themes.' . $theme . '.components.shop.item', [
+        'product' => $product,
+        'theme' => $theme,
+        '_index' => $i
+      ])
+      @endcomponent
+    </div>
+    @endforeach --}}
+  </div>
+
+  <a href="#" class="btn btn--primary">pogledajte sve</a>
+</div>
+
+<div class="client_brands">
+  <div class="container">
+    <h2 class="display-3 section-title--serif">Brendovi</h2>
+    <ul class="home_brands-list">
+      <li class="home_brands-list-item">
+        <a href="#">
+          <img
+            src="http://l4m.mia.rs/storage/uploads/brands/05-2018/boss-6-Er.png" 
+            alt="brend!">
+        </a>
+      </li>
+      <li class="home_brands-list-item">
+        <a href="#">
+          <img
+            src="http://l4m.mia.rs/storage/uploads/brands/05-2018/hermes-13-FQ.png" 
+            alt="brend!!! yeah!">
+        </a>
+      </li>
+    </div>
+  </div>
+</div>
   
-  @endsection
+@endsection
