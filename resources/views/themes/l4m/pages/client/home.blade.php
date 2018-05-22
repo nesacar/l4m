8ff6dc49f718d4166f31586d4fa742a6c92710c4
VacaSan <kotaracvladan@gmail.com>
2018-05-22 09:48:24 +0200

    Rebuild hero section

--- resources/views/themes/l4m/pages/client/home.blade.php
+++ resources/views/themes/l4m/pages/client/home.blade.php
@@ -7,8 +7,8 @@
 
 @section('content')
 
-<div class="hero hero--scrim">
-  <div class="image hero_image">  
+<div class="hero hero--scrim client_hero">
+  <div class="image hero_image">
     <picture>
       {{-- <source
         media="(min-width: 696px)"
@@ -18,34 +18,36 @@
       />
     </picture>
   </div>
-</div>
-<div class="container client_profile">
-  <div class="avatar">
-    <div class="image image--square">
-      <img
-        src="https://images.pexels.com/photos/555017/pexels-photo-555017.jpeg?auto=compress&cs=tinysrgb&h=350"
-        alt="doggy">
+  <div class="client_profile">
+    <div class="container client_profile-wrap">
+      <div class="avatar">
+        <div class="image image--square">
+          <img
+          src="https://images.pexels.com/photos/555017/pexels-photo-555017.jpeg?auto=compress&cs=tinysrgb&h=350"
+          alt="doggy">
+        </div>
+      </div>
+      <div class="client_info">
+        <h1 class="client_name">Client Name</h1>
+        <nav class="client_nav">
+          <ul class="client_nav-list">
+            <li class="client_nav-item">
+              <a href="#" class="client_nav-link">o nama</a>
+            </li>
+            <li class="client_nav-item">
+              <a href="#" class="client_nav-link active">web shop</a>
+            </li>
+            <li class="client_nav-item">
+              <a href="#" class="client_nav-link">akcije</a>
+            </li>
+            <li class="client_nav-item">
+              <a href="#" class="client_nav-link">blog</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   </div>
-  <div class="client_info">
-    <h1 class="client_name">Client Name</h1>
-    <nav class="client_nav">
-      <ul class="client_nav-list">
-        <li class="client_nav-item">
-          <a href="#" class="client_nav-link">o nama</a>
-        </li>
-        <li class="client_nav-item">
-          <a href="#" class="client_nav-link active">web shop</a>
-        </li>
-        <li class="client_nav-item">
-          <a href="#" class="client_nav-link">akcije</a>
-        </li>
-        <li class="client_nav-item">
-          <a href="#" class="client_nav-link">blog</a>
-        </li>
-      </ul>
-    </nav>
-  </div>
 </div>
-
-@endsection
+  
+  @endsection
