344f10b7f330b1ccc6edeeb362894b6c5301de94
VacaSan <kotaracvladan@gmail.com>
2018-05-24 10:19:19 +0200

    Toggles between images on hover.

--- resources/views/themes/l4m/components/shop/item.blade.php
+++ resources/views/themes/l4m/components/shop/item.blade.php
@@ -37,9 +37,9 @@
         <div class="image image--portrait lazy-image"
           data-src="{{ url(\Imagecache::get($product->image, '215x287')->src) }}"
         ></div>
-        @if(count($product->alt_image) > 0)
-        <div class="image image--portrait lazy-image"
-          data-src="{{ url(\Imagecache::get($product->alt_image->first()->file_path, '215x287')->src) }}"
+        @if(count($product->altImage))
+        <div class="image image--portrait lazy-image shop-item_alt-image"
+          data-src="{{ url(\Imagecache::get($product->altImage->first()->file_path, '215x287')->src) }}"
         ></div>
         @endif
       </div>
