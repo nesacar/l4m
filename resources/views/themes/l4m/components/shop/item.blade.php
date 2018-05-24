a9b69fac5184420ab370873cf8f2118bc45b4349
VacaSan <kotaracvladan@gmail.com>
2018-05-24 09:57:00 +0200

    Remove bugs.

--- resources/views/themes/l4m/components/shop/item.blade.php
+++ resources/views/themes/l4m/components/shop/item.blade.php
@@ -37,11 +37,6 @@
         <div class="image image--portrait lazy-image"
           data-src="{{ url(\Imagecache::get($product->image, '215x287')->src) }}"
         ></div>
-        @if(count($product->alt_image) > 0)
-        <div class="image image--portrait lazy-image"
-          data-src="{{ url(\Imagecache::get($product->alt_image->first()->file_path, '215x287')->src) }}"
-        ></div>
-        @endif
       </div>
     </div>
     <div class="shop-item_content">
