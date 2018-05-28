271b8875cb464b005bb983d210b4135c98285d27
VacaSan <kotaracvladan@gmail.com>
2018-05-28 12:43:06 +0200

    Text-field to component.

---
+++ resources/views/themes/l4m/components/text-field.blade.php
@@ -0,0 +1,5 @@
+<div class="text-field">
+  <input class="text-field_input" type="text" name="hello" id="hello">
+  <label class="text-field_label" for="hello">Hello</label>
+  <div class="text-field_line"></div>
+</div>
--- resources/views/themes/l4m/pages/registration.blade.php
+++ resources/views/themes/l4m/pages/registration.blade.php
@@ -8,11 +8,8 @@
 @section('content')
 
 <div class="container" style="padding-top: 32px;">
-  <div class="text-field">
-    <input class="text-field_input" type="text" name="hello" id="hello">
-    <label class="text-field_label" for="hello">Hello</label>
-    <div class="text-field_line"></div>
-  </div>
+  @textfield()
+  @endtextfield
 </div>
 
 @endsection
