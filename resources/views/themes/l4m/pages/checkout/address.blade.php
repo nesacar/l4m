c247a7c5648612ce22c8596a38f096849f41b36b
VacaSan <kotaracvladan@gmail.com>
2018-05-29 16:16:14 +0200

    Checkaout address markup and basic styles.

--- resources/views/themes/l4m/pages/checkout/address.blade.php
+++ resources/views/themes/l4m/pages/checkout/address.blade.php
@@ -7,35 +7,84 @@
 
 @section('content')
 <div class="container">
-  @stepper([
-    'steps' => [
-      (object)[
-        'link' => '#first',
-        'text' => 'Adresa slanja',
-        'disabled' => false,
-      ],
-      (object)[
-        'link' => '#second',
-        'text' => 'Način dostave',
-        'disabled' => false,
-      ],
-      (object)[
-        'link' => '#second',
-        'text' => 'Način plaćanja',
-        'disabled' => true,
-      ],
-      (object)[
-        'link' => '#second',
-        'text' => 'Potvrda porudžbine',
-        'disabled' => true,
-      ],
-      (object)[
-        'link' => '#second',
-        'text' => 'Kraj',
-        'disabled' => true,
-      ],
-    ],
-  ])
-  @endstepper
+  <div class="checkout">
+    <div class="checkout-steps">
+      @stepper([
+        'steps' => [
+          (object)[
+            'link' => '#first',
+            'text' => 'Adresa slanja',
+            'disabled' => false,
+          ],
+          (object)[
+            'link' => '#second',
+            'text' => 'Način dostave',
+            'disabled' => false,
+          ],
+          (object)[
+            'link' => '#second',
+            'text' => 'Način plaćanja',
+            'disabled' => true,
+          ],
+          (object)[
+            'link' => '#second',
+            'text' => 'Potvrda porudžbine',
+            'disabled' => true,
+          ],
+          (object)[
+            'link' => '#second',
+            'text' => 'Kraj',
+            'disabled' => true,
+          ],
+        ],
+      ])
+      @endstepper
+    </div>
+    <div class="checkout-content">
+      <h2 class="subtitle subtitle--sans-serif display-4">nova adresa</h2>
+      <div class="row">
+        <div class="col-md-6">
+          @textfield([
+            'id' => 'name',
+            'name' => 'ime',
+            'label' => 'Ime',
+            'required' => true,
+            'helper_text' => 'Ovo polje je obavezno',
+          ])
+          @endtextfield
+        </div>
+        <div class="col-md-6">
+          @textfield([
+            'id' => 'surname',
+            'name' => 'prezime',
+            'label' => 'Prezime',
+            'required' => true,
+            'helper_text' => 'Ovo polje je obavezno',
+          ])
+          @endtextfield
+        </div>
+        <div class="col-md-6">
+          @textfield([
+            'id' => 'address',
+            'name' => 'adresa',
+            'label' => 'Ulica i broj',
+            'required' => true,
+            'helper_text' => 'Ovo polje je obavezno',
+          ])
+          @endtextfield
+        </div>
+        <div class="col-md-6">
+          @textfield([
+            'id' => 'tel',
+            'name' => 'telefon',
+            'label' => 'Telefon',
+            'required' => true,
+            'helper_text' => 'Ovo polje je obavezno',
+          ])
+          @endtextfield
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
