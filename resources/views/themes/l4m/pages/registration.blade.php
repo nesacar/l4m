c2b8fedc8429b4058a69770e3eb0eb2df4c99615
VacaSan <kotaracvladan@gmail.com>
2018-05-29 11:39:52 +0200

    Registration page partial form.

--- resources/views/themes/l4m/pages/registration.blade.php
+++ resources/views/themes/l4m/pages/registration.blade.php
@@ -8,13 +8,61 @@
 @section('content')
 
 <div class="container" style="padding-top: 32px;">
-  @textfield([
-    'id' => 'email',
-    'name' => 'email',
-    'label' => 'email',
-    'helper_text' => 'This field is required.',
-  ])
-  @endtextfield
+  <form>
+    <div class="row">
+      <div class="col-md-6">
+        @textfield([
+          'value' => '',
+          'id' => 'name',
+          'name' => 'name',
+          'label' => 'Ime i prezime',
+          'helper_text' => 'Ovo polje je obavezno',
+          'required' => true,
+          'error' => false,
+          ])
+        @endtextfield
+      </div>
+      <div class="col-md-6">
+        @textfield([
+          'value' => '',
+          'id' => 'email',
+          'name' => 'email',
+          'label' => 'email',
+          'type' => 'email',
+          'helper_text' => 'Ovo polje je obavezno',
+          'required' => true,
+          'error' => false,
+          ])
+        @endtextfield
+      </div>
+      <div class="col-md-6">
+        @textfield([
+          'value' => '',
+          'id' => 'password',
+          'name' => 'password',
+          'label' => 'Šifra',
+          'type' => 'password',
+          'helper_text' => 'Ovo polje je obavezno',
+          'required' => true,
+          'error' => false,
+          ])
+        @endtextfield
+      </div>
+      <div class="col-md-6">
+        @textfield([
+          'value' => '',
+          'id' => 'password_confirm',
+          'name' => 'password_confirm',
+          'label' => 'Potvrdite šifru',
+          'type' => 'password',
+          'helper_text' => 'Ovo polje je obavezno',
+          'required' => true,
+          'error' => false,
+          ])
+        @endtextfield
+      </div>
+    </div>
+  </form>
 </div>
 
 @endsection
