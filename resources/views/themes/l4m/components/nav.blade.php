6cb792b9cc6100c1bf6958764d6dba9f2e6f2765
nesacar <nebojsart1409@yahoo.com>
2018-05-30 14:19:52 +0200

    menu - links

--- resources/views/themes/l4m/components/nav.blade.php
+++ resources/views/themes/l4m/components/nav.blade.php
@@ -5,43 +5,45 @@
             <a class="{{ $className }}" href="{{ url($link->link . $link->sufix) }}">{{ $link->title}}</a>
             @if(count($link->children)>0)
                 <div class="mega-menu">
-                  <div class="mega-menu_scrim"></div>
-                  <div class="mega-menu_content">
-                    <div class="container mega-menu_row">
-                      {{-- <div class="row">
-                          @foreach($link->children as $subLink)
-                              <div class="col-3 mega-menu_col">
-                                  <a href="{{ url($subLink->link . $subLink->sufix) }}"
-                                      class="mega-menu_link">{{ $subLink->title }}</a>
-                              </div>
-                              <div class="col-3 mega-menu_col">
-                                  @if($subLink->image)
-                                      <a href="{{ $subLink->link . $subLink->sufix }}" class="image image--standard">
-                                          <img src="{{ url($subLink->image) }}" alt="{{ $subLink->title }}">
-                                      </a>
-                                  @endif
-                              </div>
-                          @endforeach
-                      </div> --}}
-                      <div class="menu-list mega-menu_col">
-                        @foreach($link->children as $subLink)
-                          <a class="menu-list_item"
-                            href="{{url($subLink->link . $subLink->sufix)}}"
-                            >{{$subLink->title}}</a>
-                        @endforeach
-                      </div>
-                      <!-- demo -->
-                      <div class="demo-img-box">
-                        <div class="image image--ultra-wide">
-                          @if(!empty($link->image))
-                            <img src="{{ url($link->image) }}" alt="{{ $link->link }}">
-                          @endif
+                    <div class="mega-menu_scrim"></div>
+                    <div class="mega-menu_content">
+                        <div class="container mega-menu_row">
+                            {{-- <div class="row">
+                                @foreach($link->children as $subLink)
+                                    <div class="col-3 mega-menu_col">
+                                        <a href="{{ url($subLink->link . $subLink->sufix) }}"
+                                            class="mega-menu_link">{{ $subLink->title }}</a>
+                                    </div>
+                                    <div class="col-3 mega-menu_col">
+                                        @if($subLink->image)
+                                            <a href="{{ $subLink->link . $subLink->sufix }}" class="image image--standard">
+                                                <img src="{{ url($subLink->image) }}" alt="{{ $subLink->title }}">
+                                            </a>
+                                        @endif
+                                    </div>
+                                @endforeach
+                            </div> --}}
+                            <div class="menu-list mega-menu_col">
+                                @foreach($link->children as $subLink)
+                                    <a class="menu-list_item"
+                                       href="{{url($subLink->link . $subLink->sufix)}}"
+                                    >{{$subLink->title}}</a>
+                                @endforeach
+                            </div>
+                            <!-- demo -->
+                            @if(!empty($link->image))
+                                <div class="demo-img-box">
+                                    <div class="image image--ultra-wide">
+
+                                        <img src="{{ url($link->image) }}" alt="{{ $link->link }}">
+
+                                    </div>
+                                    <span class="demo-something">{{ $link->title }}</span>
+                                </div>
+                                <!-- /demo -->
+                            @endif
                         </div>
-                        <span class="demo-something">{{ $link->title }}</span>
-                      </div>
-                      <!-- /demo -->
                     </div>
-                  </div>
                 </div>
             @endif
         </div>
