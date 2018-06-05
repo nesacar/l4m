8cbeb7c50c259009de32c587ac4ec48582550e13
VacaSan <kotaracvladan@gmail.com>
2018-06-05 11:12:36 +0200

    Dialog styles and markup.

--- resources/views/themes/l4m/components/zoomer.blade.php
+++ resources/views/themes/l4m/components/zoomer.blade.php
@@ -1,1 +1,10 @@
-<div class="zoomer js-zoomer"></div>
+<dialog class="dialog js-dialog">
+  <div class="dialog-body">
+    <button class="icon-btn dialog-close-btn" aria-label="zatvori">
+      <svg class="icon" role="presentation">
+        <use xlink:href="#icon_close">
+      </svg>
+    </button>
+    <div class="zoomer js-zoomer"></div>
+  </div>
+</dialog>
