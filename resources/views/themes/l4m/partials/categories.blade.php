cc6f30204c5f8b651863f4587eb63158131f5919
VacaSan <kotaracvladan@gmail.com>
2018-07-02 15:40:36 +0200

    Adjusted category widths.

--- resources/views/themes/l4m/partials/categories.blade.php
+++ resources/views/themes/l4m/partials/categories.blade.php
@@ -1,6 +1,6 @@
 <div class="row home_categories">
     @foreach($categories as $category)
-        <a href="{{ $category->getLink() }}" class="col-md-3 home_category with-flare with-scrim">
+        <a href="{{ $category->getLink() }}" class="col-md-3 col-6 home_category with-flare with-scrim">
             <div class="image image--portrait home_category-image">
                 @if(!empty($category->box_image))
                     <img src="{{ url($category->box_image) }}" alt="{{ $category->title }}">
