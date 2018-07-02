a7c753e99e2c754f4e51d52e0ddb79df4e389a64
VacaSan <kotaracvladan@gmail.com>
2018-07-02 10:41:58 +0200

    Masthead renders link if no content.

--- resources/views/themes/l4m/components/homepage-carousel.blade.php
+++ resources/views/themes/l4m/components/homepage-carousel.blade.php
@@ -8,26 +8,32 @@
   @endcarouselcontrols
   <div class="container masthead-carousel is-loading overflow--hidden">
     @foreach($data as $item)
+    @php
+      $has_content = !empty($item->title)
+        && !empty($item->subtitle)
+        && !empty($item->button);
+    @endphp
     <div class="masthead-carousel_item">
-      <div class="image masthead-image masthead-image--homepage invertable-image">
-        <picture>
-          <source media="(min-width: 696px)" srcset="{{$item->slider ? url($item->slider) : url($item->image)}}">
-          <img src="{{$item->slider ? url($item->slider) : url($item->small_image)}}"/>
-        </picture>
-      </div>
+      @if(!$has_content) <a href="{{url($item->link)}}"> @endif
+        <div class="image masthead-image masthead-image--homepage invertable-image">
+          <picture>
+            <source media="(min-width: 696px)"
+              srcset="{{$item->slider ? url($item->slider) : url($item->image)}}">
+            <img src="{{$item->slider ? url($item->slider) : url($item->small_image)}}"/>
+          </picture>
+        </div>
+      @if(!$has_content) </a> @endif
+      @if($has_content)
       <div class="masthead-carousel_content--homepage">
         <div class="masthead_action-box masthead_action-box--left">
-          @if(!empty($item->title))
           <h1 class="masthead_title display-2">{!! $item->title !!}</h1>
-          @endif
-          @if(!empty($item->subtitle))
           <p class="masthead_desc">{!! $item->subtitle !!}</p>
-          @endif
-          @if(!empty($item->button))
-          <a class="btn btn--primary masthead_action" href="{{url($item->link)}}">{{$item->button}}</a>
-          @endif
+          <a class="btn btn--primary masthead_action"
+            href="{{url($item->link)}}"
+          >{{$item->button}}</a>
         </div>
       </div>
+      @endif
     </div>
     @endforeach
   </div>
