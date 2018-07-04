b4ab21f9ad4ba8a8cfef69c62b1fb87574411eb0
nesacar <nebojsart1409@yahoo.com>
2018-07-04 23:11:14 +0200

    testing shell comands

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -68,16 +68,6 @@ class PagesController extends Controller
 
     public function proba()
     {
-        $shopBar = ShopBar::find(44);
-        $shopBar->product()->sync([541, 542, 543, 544]);
-
-//        factory('App\Tag', 30)->create();
-        //return Attribute::find(41)->product;
-//        $product = Product::find(225);
-//        return $product->colors()->first();
-        //\Artisan::call('cache:clear');
-//        $category = Category::find(76);
-//        return $product = Product::find(459)->getLink($category);
-        return 'done';
+        //
     }
 }
