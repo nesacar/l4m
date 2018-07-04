905acd35caf11c7ef533d30f04f9180198887627
VacaSan <kotaracvladan@gmail.com>
2018-07-04 10:38:53 +0200

    User profile details.

--- resources/views/themes/l4m/pages/user/profile.blade.php
+++ resources/views/themes/l4m/pages/user/profile.blade.php
@@ -31,54 +31,114 @@
       </div>
       <div class="checkout-content my-4">
         <h3 class="display-4 text--sans-serif">Moj profil</h3>
-        <div>
-          @textfield([
-            'value' => 'User name',
-            'id' => 'name',
-            'name' => 'name',
-            'label' => 'Ime i prezime',
-          ])
-          @endtextfield
-          @textfield([
-            'value' => '(+381)60123456',
-            'id' => 'tel',
-            'name' => 'tel',
-            'label' => 'Telefon',
-          ])
-          @endtextfield
-          @textfield([
-            'value' => 'User address',
-            'id' => 'address',
-            'name' => 'address',
-            'label' => 'Ulica i broj',
-          ])
-          @endtextfield
-          @textfield([
-            'value' => 'City',
-            'id' => 'city',
-            'name' => 'town',
-            'label' => 'Grad',
-          ])
-          @endtextfield
-          @textfield([
-            'value' => 11000,
-            'id' => 'postcode',
-            'name' => 'postcode',
-            'label' => 'Poštanski broj',
-          ])
-          @endtextfield
-          @selectbox([
-            'id' => 'Država',
-            'name' => 'country',
-            'label' => 'Država',
-          ])
-          <option value="1">Srbija</option>
-          <option value="2">Hrvatska</option>
-          @endselectbox
-          <div>
-            <button class="btn btn--primary">izmeni</button>
+        <form class="pb-4">
+          <div class="mb-2">
+            @textfield([
+              'value' => 'User name',
+              'id' => 'name',
+              'name' => 'name',
+              'label' => 'Ime i prezime',
+              'helper_text' => 'Ovo polje je obavezno',
+              'required' => true,
+              'error' => false,
+            ])
+            @endtextfield
+            @textfield([
+              'value' => '(+381)60123456',
+              'id' => 'tel',
+              'name' => 'tel',
+              'label' => 'Telefon',
+              'helper_text' => 'Ovo polje je obavezno',
+              'required' => true,
+              'error' => false,
+            ])
+            @endtextfield
+            @textfield([
+              'value' => 'User address',
+              'id' => 'address',
+              'name' => 'address',
+              'label' => 'Ulica i broj',
+              'helper_text' => 'Ovo polje je obavezno',
+              'required' => true,
+              'error' => false,
+            ])
+            @endtextfield
+            @textfield([
+              'value' => 'City',
+              'id' => 'city',
+              'name' => 'town',
+              'label' => 'Grad',
+              'helper_text' => 'Ovo polje je obavezno',
+              'required' => true,
+              'error' => false,
+            ])
+            @endtextfield
+            @textfield([
+              'value' => 11000,
+              'id' => 'postcode',
+              'name' => 'postcode',
+              'label' => 'Poštanski broj',
+              'helper_text' => 'Ovo polje je obavezno',
+              'required' => true,
+              'error' => false,
+            ])
+            @endtextfield
+            @selectbox([
+              'id' => 'Država',
+              'name' => 'country',
+              'label' => 'Država',
+              'helper_text' => 'Ovo polje je obavezno',
+              'required' => true,
+              'error' => false,
+            ])
+            <option value="1">Srbija</option>
+            <option value="2">Hrvatska</option>
+            @endselectbox
           </div>
-        </div>
+          <div>
+            <button class="btn btn--primary">Promeni podatke</button>
+          </div>
+        </form>
+        <form class="pb-4">
+          <div class="mb-2">
+            @textfield([
+              'value' => 'stara lozinka',
+              'id' => 'password',
+              'name' => 'password',
+              'label' => 'Lozinka',
+              'type' => 'password',
+              'helper_text' => 'Ovo polje je obavezno',
+              'required' => true,
+              'error' => false,
+              ])
+            @endtextfield
+            @textfield([
+              'value' => '',
+              'id' => 'new_password',
+              'name' => 'new_password',
+              'label' => 'Nova lozinka',
+              'type' => 'password',
+              'helper_text' => 'Ovo polje je obavezno',
+              'required' => true,
+              'error' => false,
+              ])
+            @endtextfield
+            @textfield([
+              'value' => '',
+              'id' => 'repeat_password',
+              'name' => 'repeat_password',
+              'label' => 'Ponovite lozinku',
+              'type' => 'password',
+              'helper_text' => 'Ovo polje je obavezno',
+              'required' => true,
+              'error' => false,
+              ])
+            @endtextfield
+          </div>
+          <div>
+            <button class="btn btn--primary">Promeni lozinku</button>
+          </div>
+        </form>
       </div>
     </div>
   </div>
