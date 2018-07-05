981b425c8ce0485fc3f84d34bdd23cf1d01af8b2
VacaSan <kotaracvladan@gmail.com>
2018-07-05 11:48:55 +0200

    Copies the src email files.

---
+++ copy.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+const srcDir = path.join(__dirname, '/email/src');
+const destDir = path.join(__dirname, '/email/dist');
+
+// clean the dist folder
+fs.readdirSync(destDir)
+  .forEach((file) => {
+    fs.unlinkSync(`${destDir}/${file}`);
+  });
+
+// copy the source files to the dist folder.
+fs.readdirSync(srcDir)
+  .forEach((srcFile) => {
+    const content = fs.readFileSync(`${srcDir}/${srcFile}`, 'utf8');
+    const destFile = srcFile.replace(/\.mjml$/, '.blade.php');
+    fs.writeFileSync(`${destDir}/${destFile}`, content, 'utf8');
+  });
