959f5c60e1c358b41ebe1b038d58152e688dbdfd
pancevac <sinisa.buncic7@gmail.com>
2018-07-30 14:14:31 +0200

    Fixed wrong column type in migration

--- database/migrations/2018_07_30_090338_add_category_id_to_posts_table.php
+++ database/migrations/2018_07_30_090338_add_category_id_to_posts_table.php
@@ -14,7 +14,7 @@ class AddCategoryIdToPostsTable extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->tinyInteger('category_id')->nullable()->after('brand_id');
+            $table->integer('category_id')->nullable()->after('brand_id');
         });
     }
 
