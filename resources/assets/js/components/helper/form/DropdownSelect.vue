b889eeed5c76c6610e1eb7e3cace00331022ed2c
pancevac <sinisa.buncic7@gmail.com>
2018-08-03 12:14:36 +0200

    Dropdown select component.

---
+++ resources/assets/js/components/helper/form/DropdownSelect.vue
@@ -0,0 +1,38 @@
+<template>
+    <select :name="name" @change="onChange">
+        <option value="0" :selected=true>{{value}}</option>
+        <option
+                :value="option.id"
+                v-for="(option, index) in options"
+        >{{ option.title }}</option>
+    </select>
+</template>
+
+<script>
+    export default {
+        props: {
+            name: {
+                type: String,
+                required: false,
+                default: 'name-id',
+            },
+            onChange: {
+                type: Function,
+                required: true,
+            },
+            value: {
+                type: String,
+                default: '',
+            },
+            /**
+             * {Object[]} options
+             * {string|number} options.id
+             * {string|number} options.title
+             */
+            options: {
+                type: Array,
+                required: true,
+            },
+        },
+    };
+</script>
