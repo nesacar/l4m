49194379457eb3e6b880342d568d9a7be11d2f20
pancevac <sinisa.buncic7@gmail.com>
2018-08-07 11:33:34 +0200

    Refactor DropdownSelect.

--- resources/assets/js/components/helper/form/DropdownSelect.vue
+++ resources/assets/js/components/helper/form/DropdownSelect.vue
@@ -1,9 +1,9 @@
 <template>
-    <select :name="name" @change="onChange">
-        <option value="0" :selected=true>{{value}}</option>
-        <option
-                :value="option.id"
-                v-for="(option, index) in options"
+    <select :name="name" @change="_handleChange">
+        <option value="-1" :selected=true>{{_value}}</option>
+        <option :key="option.id"
+            :value="option.id"
+            v-for="option in options"
         >{{ option.title }}</option>
     </select>
 </template>
@@ -16,14 +16,22 @@
                 required: false,
                 default: 'name-id',
             },
+
             onChange: {
                 type: Function,
                 required: true,
             },
+
+            /**
+             * {Object[]} options
+             * {string|number} options.id
+             * {string|number} options.title
+             */
             value: {
-                type: String,
-                default: '',
+                type: Array,
+                default: [],
             },
+
             /**
              * {Object[]} options
              * {string|number} options.id
@@ -32,6 +40,45 @@
             options: {
                 type: Array,
                 required: true,
+            },
+        },
+
+        computed: {
+            _value() {
+                const values = this.options
+                    .filter((option) => this.value.includes(option.id))
+                    .map((option) => option.title);
+
+                return values.length
+                    ? values.join(', ')
+                    : `Izaberite ${this.name}`;
+            },
+        },
+
+        methods: {
+            _handleChange(evt) {
+                const id = parseInt(evt.target.value);
+                const index = this.value.indexOf(id);
+                let value = [];
+
+                if (index < 0) {
+                    // add to `value`,
+                    value = this.value.concat(id);
+                } else {
+                    // remove from `value`
+                    value = [
+                        ...this.value.slice(0, index),
+                        ...this.value.slice(index + 1),
+                    ];
+                }
+
+                // props handler
+                this.onChange({
+                    target: {
+                        name: this.name,
+                        value,
+                    },
+                });
             },
         },
     };
