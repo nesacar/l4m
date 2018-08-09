82ae33d2492a05c87a1cac8b62e25728fc385521
VacaSan <kotaracvladan@gmail.com>
2018-08-09 08:46:29 +0200

    Table format.

--- resources/assets/js/components/products/ProductTableCreate.vue
+++ resources/assets/js/components/products/ProductTableCreate.vue
@@ -40,262 +40,261 @@
         </div>
     </div>
 </template>
+
 <script>
+  import PreviewImage from '../helper/PreviewImage.vue';
+  import DropdownSelect from '../helper/form/DropdownSelect.vue';
+  import moment from 'moment';
+  import swal from 'sweetalert2';
+  import DemoGenericInput from '../helper/form/DemoForm/DemoGenericInput.vue';
+  import {STATIC_FIELDS} from '../helper/form/DemoForm/static-fields';
 
-    import PreviewImage from '../helper/PreviewImage.vue';
-    import DropdownSelect from '../helper/form/DropdownSelect.vue';
-    import moment from 'moment';
-    import swal from 'sweetalert2';
-    import DemoGenericInput from '../helper/form/DemoForm/DemoGenericInput.vue';
-    import {STATIC_FIELDS} from '../helper/form/DemoForm/static-fields';
+  let staticDataTemplate = Object.assign({}, STATIC_FIELDS);
 
-    let staticDataTemplate = Object.assign({}, STATIC_FIELDS);
+  /**
+   * Maps over the given object.
+   *
+   * @param {Object} obj
+   * @param {Function} fn
+   * @return {Object}
+   */
+  const mapObject = (obj, fn) => {
+    return Object.keys(obj).reduce((acc, key) => ({
+      ...acc,
+      [key]: fn(key, obj[key]),
+    }), {});
+  };
 
-    /**
-     * Maps over the given object.
-     *
-     * @param {Object} obj
-     * @param {Function} fn
-     * @return {Object}
-     */
-    const mapObject = (obj, fn) => {
-        return Object.keys(obj).reduce((acc, key) => ({
-            ...acc,
-            [key]: fn(key, obj[key]),
-        }), {});
-    };
+  export default {
+    data() {
+      return {
+        showTable: false,
+        sets: [],
+        selected: -1,
+        fields: [],
+        error: null,
+      };
+    },
 
-    export default {
-        data() {
-            return {
-                showTable: false,
-                sets: [],
-                selected: -1,
-                fields: [],
-                error: null,
-            };
-        },
+    components: {
+      'preview-image': PreviewImage,
+      'dropdown-select': DropdownSelect,
+      'demo-generic-input': DemoGenericInput,
+    },
 
-        components: {
-            'preview-image': PreviewImage,
-            'dropdown-select': DropdownSelect,
-            'demo-generic-input': DemoGenericInput,
-        },
+    computed: {
+      formFields() {
+        return this.fields.map((field) => {
+          return Object.keys(field).map((name) => ({
+            ...field[name],
+            name,
+          }));
+        });
+      },
 
-        computed: {
-            formFields() {
-                return this.fields.map((field) => {
-                    return Object.keys(field).map((name) => ({
-                        ...field[name],
-                        name,
-                    }));
-                });
-            },
+      admin() {
+        return this.$store.getters.isAdmin;
+      },
 
-            admin(){
-                return this.$store.getters.isAdmin;
-            },
+      user() {
+        return this.$store.getters.getUser;
+      },
 
-            user(){
-                return this.$store.getters.getUser;
-            },
+      publish_at() {
+        return this.product.date + ' ' + this.product.time
+      },
+    },
 
-            publish_at(){
-                return this.product.date + ' ' + this.product.time
-            },
-        },
+    mounted() {
+      this.getSets();
+      this.getStaticData();
+    },
 
-        mounted() {
-            this.getSets();
-            this.getStaticData();
-            this.getTest();
-        },
+    methods: {
+      submit() {
+        this.product.user_id = this.user.id;
+        this.product.publish_at = this.publish_at;
 
-        methods: {
-            getTest(){
-                axios.get('api/products/tableList')
-                    .then(res => {
-                        console.log(res.data);
-                    })
-            },
-            submit() {
-                this.product.user_id = this.user.id;
-                this.product.publish_at = this.publish_at;
-                axios.post('api/products/saveProduct', this.product)
-                    .then(res => {
-                        swal({
-                            position: 'center',
-                            type: 'success',
-                            title: 'Success',
-                            showConfirmButton: false,
-                            timer: 1500
-                        });
-                        this.$router.push('/products/table');
-                    }).catch(e => {
-                    console.log(e.response);
-                    this.error = e.response.data.errors;
-                })
-            },
+        axios.post('api/products/saveProduct', this.product)
+          .then((res) => {
+            swal({
+              position: 'center',
+              type: 'success',
+              title: 'Success',
+              showConfirmButton: false,
+              timer: 1500,
+            });
+            
+            this.$router.push('/products/table');
+          })
+          .catch((rr) => {
+            console.error(err);
+            this.error = err.response.data.errors;
+          });
+      },
 
-            getStaticData() {
-                Promise.all([
-                    axios.get('api/clients/lists'),
-                    axios.get('api/brands/lists'),
-                    axios.get('api/categories/listsSelect'),
-                ])
-                    .then((p) => p.map((p) => p.data))
-                    .then((promises) => {
-                        return promises.reduce((acc, cur) => {
-                            return {
-                                ...acc,
-                                ...cur,
-                            };
-                        }, {});
-                    })
-                    .then((data) => {
-                        staticDataTemplate = mapObject(staticDataTemplate, (key, value) => {
-                            if (data[key]) {
-                                return {
-                                    ...value,
-                                    options: ((key !== 'categories') ? [{id: -1, title: `Izaberi ${key}`}] : []).concat(data[key]),
-                                };
-                            }
-
-                            return value;
-                        });
-                    })
-                    .catch((err) => {
-                        this.error = err.response.data.errors;
-                        console.error(err);
-                    });
-            },
-
-            setBrand(event) {
-                let brand_id = event.target.value;
-                this.getCollections(brand_id);
-            },
-
-            getCollections(id, index) {
-                axios.get(`api/collections/lists?brand_id=${id}`)
-                    .then((r) => r.data)
-                    .then((data) => {
-                        const product = Object.assign({}, this.fields[index], {
-                            collections: {
-                                ...this.fields[index].collections,
-                                options: [{id: -1, title: 'Izaberi kolekciju'}].concat(data.collections.map((c) => ({
-                                    id: c.id,
-                                    title: c.title,
-                                }))),
-                            },
-                        });
-
-                        this.fields = [
-                            ...this.fields.slice(0, index),
-                            product,
-                            ...this.fields.slice(index + 1),
-                        ];
-                    })
-                    .catch((err) => {
-                        console.error(err);
-                    });
-            },
-
-            getSets() {
-                axios.get('api/sets')
-                    .then((res) => {
-                        this.sets = res.data.lists;
-                    })
-                    .catch((err) => {
-                        this.error = err.response.data.errors;
-                        console.log(err);
-                    });
-            },
-
-            /* Event handlers */
-            onInputChange(index) {
-                return (evt) => {
-                    const {name, value} = evt.target;
-
-                    this.fields[index][name] = {
-                        ...this.fields[index][name],
-                        value,
-                    };
-
-                    if (name === 'brands') {
-                        this.getCollections(value, index);
-                    }
+      getStaticData() {
+        Promise.all([
+          axios.get('api/clients/lists'),
+          axios.get('api/brands/lists'),
+          axios.get('api/categories/listsSelect'),
+        ])
+          .then((p) => p.map((p) => p.data))
+          .then((promises) => {
+            return promises.reduce((acc, cur) => {
+              return {
+                ...acc,
+                ...cur,
+              };
+            }, {});
+          })
+          .then((data) => {
+            staticDataTemplate = mapObject(staticDataTemplate, (key, value) => {
+              if (data[key]) {
+                return {
+                  ...value,
+                  options: ((key !== 'categories') ? [{id: -1, title: `Izaberi ${key}`}] : [])
+                    .concat(data[key]),
                 };
-            },
+              }
 
-            onChange() {
-                const id = this.selected;
-                // Clear previously generated fields
-                this.fields = [];
+              return value;
+            });
+          })
+          .catch((err) => {
+            this.error = err.response.data.errors;
+            console.error(err);
+          });
+      },
 
-                axios.get(`api/products/${id}/table`)
-                    .then((res) => {
-                        this.showTable = true;
+      setBrand(event) {
+        let brand_id = event.target.value;
+        this.getCollections(brand_id);
+      },
 
-                        const setProps = res.data.setProperties;
+      getCollections(id, index) {
+        axios.get(`api/collections/lists?brand_id=${id}`)
+          .then((r) => r.data)
+          .then((data) => {
+            const product = Object.assign({}, this.fields[index], {
+              collections: {
+                ...this.fields[index].collections,
+                options: [{id: -1, title: 'Izaberi kolekciju'}]
+                  .concat(data.collections.map((c) => ({
+                    id: c.id,
+                    title: c.title,
+                  }))),
+              },
+            });
 
-                        const dynamicFields = setProps.reduce((acc, cur) => {
-                            return {
-                                ...acc,
-                                [`dynamic_${cur.slug}`]: {
-                                    type: 'select',
-                                    value: -1,
-                                    options: [{id: -1, title: `Izaberi ${cur.title}`}].concat(cur.attribute.map((attr) => ({
-                                            id: attr.id,
-                                            title: attr.title,
-                                        }))),
-                                },
-                            };
-                        }, {});
+            this.fields = [
+              ...this.fields.slice(0, index),
+              product,
+              ...this.fields.slice(index + 1),
+            ];
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+      },
 
-                        this.fields = this.fields.concat({
-                            ...staticDataTemplate,
-                            ...dynamicFields,
-                        });
-                    })
-                    .catch((err) => {
-                        this.error = err.response.data.errors;
-                        console.error(err);
-                    });
-            },
+      getSets() {
+        axios.get('api/sets')
+          .then((res) => {
+            this.sets = res.data.lists;
+          })
+          .catch((err) => {
+            this.error = err.response.data.errors;
+            console.error(err);
+          });
+      },
 
-            onAddNewRow() {
-                // last elements index.
-                const index = this.fields.length - 1;
-                const lastItemCopy = Object.assign({}, this.fields[index]);
-                this.fields = this.fields.concat(lastItemCopy);
-            },
+      /* Event handlers */
+      onInputChange(index) {
+        return (evt) => {
+          const {name, value} = evt.target;
 
-            onSubmit() {
-                // Merge user info in fields
-                const payload = {
-                    fields: this.fields,
-                    user_id: this.user.id,
-                };
+          this.fields[index][name] = {
+            ...this.fields[index][name],
+            value,
+          };
 
-                axios.post('api/products/tableCreate', payload)
-                    .then(res => {
-                        console.log(res.data);
-                        swal({
-                            position: 'center',
-                            type: 'success',
-                            title: 'Uspeh, ',
-                            text: `Broj kreiranih proizvoda: ${res.data.new}, broj izmenjenih proizvoda: ${res.data.old}`,
-                            showConfirmButton: false,
-                            timer: 5000
-                        });
-                    })
-                    .catch(err => {
-                        console.error(err);
-                    })
-            },
-        }
+          if (name === 'brands') {
+            this.getCollections(value, index);
+          }
+        };
+      },
+
+      onChange() {
+        const id = this.selected;
+        // Clear previously generated fields
+        this.fields = [];
+
+        axios.get(`api/products/${id}/table`)
+          .then((res) => {
+            this.showTable = true;
+
+            const setProps = res.data.setProperties;
+
+            const dynamicFields = setProps.reduce((acc, cur) => {
+              return {
+                ...acc,
+                [`dynamic_${cur.slug}`]: {
+                  type: 'select',
+                  value: -1,
+                  options: [{id: -1, title: `Izaberi ${cur.title}`}]
+                    .concat(cur.attribute.map((attr) => ({
+                      id: attr.id,
+                      title: attr.title,
+                    }))),
+                },
+              };
+            }, {});
+
+            this.fields = this.fields.concat({
+              ...staticDataTemplate,
+              ...dynamicFields,
+            });
+          })
+          .catch((err) => {
+            this.error = err.response.data.errors;
+            console.error(err);
+          });
+      },
+
+      onAddNewRow() {
+        // last elements index.
+        const index = this.fields.length - 1;
+        const lastItemCopy = Object.assign({}, this.fields[index]);
+        this.fields = this.fields.concat(lastItemCopy);
+      },
+
+      onSubmit() {
+        // Merge user info in fields
+        const payload = {
+          fields: this.fields,
+          user_id: this.user.id,
+        };
+
+        axios.post('api/products/tableCreate', payload)
+          .then(res => {
+            swal({
+              position: 'center',
+              type: 'success',
+              title: 'Uspeh, ',
+              text: `Broj kreiranih proizvoda: ${res.data.new}, broj izmenjenih proizvoda: ${res.data.old}`,
+              showConfirmButton: false,
+              timer: 5000
+            });
+          })
+          .catch(err => {
+            console.error(err);
+          });
+      },
     }
+  }
 </script>
+
 <style <style lang="scss" scoped>
   .overflow {
     overflow: auto;
