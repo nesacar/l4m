dbef09fff98a5250b7e73df26cde064a24fb8795
VacaSan <kotaracvladan@gmail.com>
2018-08-13 12:47:35 +0200

    Landing page layout.

--- resources/views/themes/l4m/pages/landing.blade.php
+++ resources/views/themes/l4m/pages/landing.blade.php
@@ -13,6 +13,46 @@
     ])
   </section>
 
+  <section class="container">
+    <h2 class="display-3 section-title--serif">Žene</h2>
+    @include('themes.' . $theme . '.partials.tab-list', [
+      'options' => (object)[
+        'id' => 'zene'
+      ],
+      'bars' => $featuredProducts
+    ])
+  </section>
+
+  <section class="container">
+    <h2 class="display-3 section-title--serif">Muškarci</h2>
+    @include('themes.' . $theme . '.partials.tab-list', [
+      'options' => (object)[
+        'id' => 'muskarci'
+      ],
+      'bars' => $featuredProducts
+    ])
+  </section>
+
+  <section class="container">
+    <h2 class="display-3 section-title--serif">Deca</h2>
+    @include('themes.' . $theme . '.partials.tab-list', [
+      'options' => (object)[
+        'id' => 'deca'
+      ],
+      'bars' => $featuredProducts
+    ])
+  </section>
+
+  <section class="container">
+    <h2 class="display-3 section-title--serif">Living</h2>
+    @include('themes.' . $theme . '.partials.tab-list', [
+      'options' => (object)[
+        'id' => 'living'
+      ],
+      'bars' => $featuredProducts
+    ])
+  </section>
+
   @if(count($brands)>0)
       <section class="home_brands">
           <div class="container">
@@ -29,5 +69,26 @@
           </div>
       </section>
   @endif
+
+  <section class="container">
+    <h2 class="display-3 section-title--serif">Istaknuti proizvodi</h2>
+    @include('themes.' . $theme . '.partials.tab-list', [
+      'options' => (object)[
+        'id' => 'featured',
+      ],
+      'bars' => $featuredProducts
+    ])
+  </section>
+
+  <section class="home_discover-more">
+    <div class="container">
+        <h2 class="display-3 home_discover-more_title">Pogledajte više</h2>
+        @component('themes.' . $theme . '.components.grid', [
+          'component' => 'blog.tile',
+          'items' => $posts,
+        ])
+        @endcomponent
+    </div>
+  </section>
 
 @endsection
